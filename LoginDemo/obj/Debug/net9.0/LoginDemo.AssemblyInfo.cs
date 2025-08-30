ac9c9fba201bb2392102d932ec114f3d99362d6d
irushinw <194580878+irushinw@users.noreply.github.com>
2025-08-30 23:09:05 +0530

    Added a comment

--- LoginDemo/obj/Debug/net9.0/LoginDemo.AssemblyInfo.cs
+++ LoginDemo/obj/Debug/net9.0/LoginDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LoginDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3af6a6b3b1001f48c41fe4424486df33352e1cc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22089ce3d22867ef0d8dec5e892e9b13d2291c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("LoginDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LoginDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
